81ee12772866e0c9966a22eefab18aa06f1b322e
Ashutosh <akkiashusms@gmail.com>
2026-05-19 20:47:57 +0530

    Asian guy updated

---
+++ src/test/java/com/miscPrograms/DemoTest.java
@@ -0,0 +1,13 @@
+package com.miscPrograms;
+
+public class DemoTest {
+
+	public static void main(String[] args) {
+
+		int a = 10 ;
+		int b = 20 ;
+		
+		System.out.println("Sum of num :"+(a+b));
+	}
+
+}
