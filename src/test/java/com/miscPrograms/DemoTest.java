8ef3fc1f2759ef4d17b48f495b3bc60ce0cf8385
Ashutosh <akkiashusms@gmail.com>
2026-05-20 01:59:52 +0530

    American Y team Architech commit

--- src/test/java/com/miscPrograms/DemoTest.java
+++ src/test/java/com/miscPrograms/DemoTest.java
@@ -1,16 +1,33 @@
 package com.miscPrograms;
 
+import org.testng.annotations.Test;
+
 public class DemoTest {
+	
+	int a = 10;
+	int b = 20;
+	int c = 0 ;
 
-	public static void main(String[] args) {
+	@Test
+	public void sum() {
 
-		int a = 10 ;
-		int b = 20 ;
-		
-		System.out.println("Sum of num :"+(a+b));
+		System.out.println("Sum of num :" + (a + b));
 		System.out.println(a);
 		System.out.println(b);
 		System.out.println("Hello Sum");
 	}
 
+	@Test
+	public void subtarct()
+	{
+		c = b - a ;
+		System.out.println("Subtract of 2 num : "+c);
+	}
+	
+	@Test
+	public void multiply()
+	{
+		c = a*b ;
+		System.out.println("Multiply of 2 num : "+c);
+	}
 }
